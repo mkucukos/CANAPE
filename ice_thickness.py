d8f6d5c22554ba9d057814a94f1007bad6269553
mkucukos <94495145+mkucukos@users.noreply.github.com>
2022-01-10 00:45:32 -0800

    This code was subjected to flake8 and pep8 guidelines.

--- ice_thickness.py
+++ ice_thickness.py
@@ -1,17 +1,17 @@
 # import pandas and numpy
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import scipy.io
-
+import pandas as pd
 
 # crete a sample dataframe for Mooring_T1
 data = pd.DataFrame({
 
-    'yearday' :     [320, 330, 340, 350, 360, 370, 380,390,400,410],
-    'mean_ice_thickness' : [0.51,0.62,0.68,0.73,1.23,0.81,1,1.35,1.24,1.04],
-    'max_ice_thikcness' :    [7, 5.92, 9.31, 4.83, 11.3, 8.28,7.61,8.85,11.34,7.37],
-    'std_ice_thickness' :  [0.66, 0.44, 0.46, 1.21, 0.71, 0.51,0.66,1.05,1.13,0.57],
+    'yearday': [320, 330, 340, 350, 360, 370, 380, 390, 400, 410],
+    'mean_ice_thickness': [0.51, 0.62, 0.68, 0.73, 1.23,
+                           0.81, 1, 1.35, 1.24, 1.04],
+    'max_ice_thikcness': [7, 5.92, 9.31, 4.83, 11.3, 8.28,
+                          7.61, 8.85, 11.34, 7.37],
+    'std_ice_thickness': [0.66, 0.44, 0.46, 1.21, 0.71,
+                          0.51, 0.66, 1.05, 1.13, 0.57],
 })
 
 # view the ice thickness
@@ -19,14 +19,14 @@ data = pd.DataFrame({
 # select mean ice thickness larger than 0.75
 data.loc[data.mean_ice_thickness >= 0.75]
 
-yearday=[320, 330, 340, 350, 360, 370, 380,390,400,410]
-mean_ice_thickness=[0.51,0.62,0.68,0.73,1.23,0.81,1,1.35,1.24,1.04]
-std=[0.66, 0.44, 0.46, 1.21, 0.71, 0.51,0.66,1.05,1.13,0.57]
+yearday = [320, 330, 340, 350, 360, 370, 380, 390, 400, 410]
+mean_ice_thickness = [0.51, 0.62, 0.68, 0.73, 1.23, 0.81, 1, 1.35, 1.24, 1.04]
+std = [0.66, 0.44, 0.46, 1.21, 0.71, 0.51, 0.66, 1.05, 1.13, 0.57]
 
-plt.plot(yearday,mean_ice_thickness,'bo-')
+plt.plot(yearday, mean_ice_thickness, 'bo-')
 
 plt.errorbar(yearday, mean_ice_thickness, std, marker='s', mfc='red',
-         mec='green', ms=5, mew=4)
+             mec='green', ms=5, mew=4)
 
 
 plt.xlabel('yearday')
